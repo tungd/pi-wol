cd583be588107aee3e7a86a284d6d1716ee76050
Tung Dao <me@tungdao.com>
2016-12-18 00:34:50 +0700

    delete machine

--- main.go
+++ main.go
@@ -15,6 +15,7 @@ type Machine struct {
 	Name string `json:"name"`
 	Ip string `json:"ip"`
 	MacAddress string `json:"mac"`
+	Up bool `json:"up"`
 }
 
 type App struct {
@@ -37,6 +38,7 @@ func (app *App) Write(ms []Machine) {
 }
 
 func (app *App) listMachine(c echo.Context) error {
+	// TODO: append machine status
 	return c.JSON(http.StatusOK, app.Read())
 }
 
@@ -45,9 +47,22 @@ func (app *App) createMachine(c echo.Context) error {
 	if err := c.Bind(&m); err != nil {
 		return c.JSON(http.StatusBadRequest, err)
 	}
+
 	machines := append(app.Read(), m)
 	app.Write(machines)
 	return c.JSON(http.StatusCreated, machines)
+}
+
+func (app *App) deleteMachine(c echo.Context) error {
+	ip := c.Param("ip")
+	machines := []Machine{}
+	for _, m := range app.Read() {
+		if m.Ip != ip {
+			machines = append(machines, m)
+		}
+	}
+	app.Write(machines)
+	return c.JSON(http.StatusOK, machines)
 }
 
 func main() {
@@ -59,6 +74,7 @@ func main() {
 
 	e.GET("/api/v1/machines", app.listMachine)
 	e.POST("/api/v1/machines", app.createMachine)
+	e.DELETE("/api/v1/machines/:ip", app.deleteMachine)
 
 	assets := http.FileServer(rice.MustFindBox("./assets").HTTPBox())
 	e.GET("/*", echo.WrapHandler(assets))
