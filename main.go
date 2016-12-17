49b2cca6a40c342e5727af153fa87f46d3179300
Tung Dao <me@tungdao.com>
2016-12-17 23:10:45 +0700

    add middleware

--- main.go
+++ main.go
@@ -4,17 +4,18 @@ import (
 	"net/http"
 
 	"github.com/labstack/echo"
-	"github.com/labstack/gommon/log"
+	"github.com/labstack/echo/middleware"
 )
 
 
 func main() {
 	e := echo.New()
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
 
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello, World!")
 	})
 
-	e.Logger.SetLevel(log.INFO)
 	e.Logger.Fatal(e.Start(":5000"))
 }
